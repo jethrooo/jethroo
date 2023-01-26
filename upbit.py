b273d44157f3d6ab5912eec9e0ec290d3a0f860d
jethrooo <34976141+jethrooo@users.noreply.github.com>
2023-01-26 16:38:13 +0900

    Update upbit.py

--- upbit.py
+++ upbit.py
@@ -45,7 +45,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.4)
+            target_price = get_target_price("KRW-BTC", 0.5)                   ##### K값 #####
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
