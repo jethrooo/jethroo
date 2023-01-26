4d54496c802e98cd42e4ae523110294ccbd13d5f
jethrooo <34976141+jethrooo@users.noreply.github.com>
2023-01-26 23:53:01 +0900

    Update bitcoinAutoTradeWithAI.py

--- bitcoinAutoTradeWithAI.py
+++ bitcoinAutoTradeWithAI.py
@@ -4,8 +4,8 @@ import datetime
 import schedule
 from fbprophet import Prophet
 
-access = "aWiEURnAVOOyjfKTNz6JbPThhyEVjP8um7QY1nnb"
-secret = "ljf475vOZZkE5xfUQYUCaEftxUXakHgtUUi489vX"
+access = "Vxrvj847pkOJEpN2ObcvbfqLMium80O7TG1xZQkC"
+secret = "PoVDSPxRCxmxN1E9T2MI1sh74sxKuU4QC2NYAbGT"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
