900e4033ce812d2afbe8712fb999604688df0fc7
jethrooo <34976141+jethrooo@users.noreply.github.com>
2023-01-30 12:17:12 +0900

    Update upbit.py

--- upbit.py
+++ upbit.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = ""
-secret = ""
+access = "mejd4ROJxCDwhD5j83Myeeq0Dn66vVM3STAvix8B"
+secret = "rYKGwtIxhUGxROPF2obLVdEAaaWiADHAxphwoiJM"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
