2bdb93e6977436fe66e401a91649bd8088093659
jethrooo <34976141+jethrooo@users.noreply.github.com>
2023-02-03 19:35:07 +0900

    Delete s1218_2.py

--- s1218_2.py
+++
@@ -1,9 +0,0 @@
-# 파일명: s1218_2.py
-
-money = int(input("소지금액 입력: "))
-card  = input("카드 소지여부[yes/no]: ").lower()
-
-if money >= 4000 or card == "yes":
-    print("택시를 타고 가라")
-else:
-    print("걸어 가라")
